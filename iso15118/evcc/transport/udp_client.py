3926ae528a5fc50c9322ab15958630c559b0e621
tropxy <andre14x@gmail.com>
2021-12-14 16:57:43 +0000

    fixed grammar and context

--- iso15118/evcc/transport/udp_client.py
+++ iso15118/evcc/transport/udp_client.py
@@ -67,10 +67,10 @@ class UDPClient(DatagramProtocol):
         sock.setsockopt(socket.IPPROTO_IPV6, socket.IPV6_MULTICAST_HOPS, ttl)
 
         # Restrict multicast operation to the given interface
-        # The IP_MULTICAST_IF or IPV6_MULTICAST_IF settings, tell the socket
+        # The IP_MULTICAST_IF or IPV6_MULTICAST_IF settings tell the socket
         # which interface it shall send its multicast packets. It can be seen
-        # as the dual of bind(), since bind() controls which interface(s) the
-        # socket receives multicast packets from.
+        # as the dual of bind(), in the server side, since bind() controls which
+        # interface(s) the socket receives multicast packets from.
         interface_index = socket.if_nametoindex(NETWORK_INTERFACE)
         sock.setsockopt(socket.IPPROTO_IPV6, socket.IPV6_MULTICAST_IF, interface_index)
 
