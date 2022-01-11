ffb9eac8f4585720d5e31f11839606eaebd9ae1e
Martin Bachmann <martin.bachmann@designwerk.com>
2022-01-11 12:18:25 +0100

    ability to add existing EVSEController

--- iso15118/secc/comm_session_handler.py
+++ iso15118/secc/comm_session_handler.py
@@ -66,7 +66,7 @@ class SECCCommunicationSession(V2GCommunicationSession):
         transport: Tuple[StreamReader, StreamWriter],
         session_handler_queue: asyncio.Queue,
         config: Config,
-        evse_controller: EVSEControllerInterface,
+        evse_controller: Optional[EVSEControllerInterface] = None
     ):
         # Need to import here to avoid a circular import error
         # pylint: disable=import-outside-toplevel
@@ -76,7 +76,10 @@ class SECCCommunicationSession(V2GCommunicationSession):
 
         self.config = config
         # The EVSE controller that implements the interface EVSEControllerInterface
-        self.evse_controller = evse_controller
+        if evse_controller:
+            self.evse_controller = evse_controller
+        else:
+            self.evse_controller: EVSEControllerInterface = config.evse_controller()
         # The authorization option(s) offered with ServiceDiscoveryRes in
         # ISO 15118-2 and with AuthorizationSetupRes in ISO 15118-20
         self.offered_auth_options: Optional[List[AuthEnum]] = []
