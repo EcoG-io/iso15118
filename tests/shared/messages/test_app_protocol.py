415588b58b80b98d87ce86f661f921634350bfd0
Martin Bachmann <martin.bachmann@designwerk.com>
2022-07-29 10:42:31 +0200

    Real World test data to verify message creation for din, iso15118-2 and app protocol

---
+++ tests/shared/messages/test_app_protocol.py
@@ -0,0 +1,26 @@
+import json
+from iso15118.shared.exi_codec import CustomJSONDecoder
+from iso15118.shared.messages.app_protocol import SupportedAppProtocolRes, SupportedAppProtocolReq
+
+
+# Test json strings recorded 28.7.2022 with Comemso Multi Mobile DC protocol tester
+class TestAppProtocol_MessageCreation:
+
+    def test_common_SupportedAppProtocolRes_messages_can_be_parsed(self):
+        decoded_dict = json.loads('{"supportedAppProtocolRes": {"ResponseCode": "OK_SuccessfulNegotiation", '
+                                  '"SchemaID": 0}}',
+                                  cls=CustomJSONDecoder)
+
+        message = SupportedAppProtocolRes.parse_obj(decoded_dict["supportedAppProtocolRes"])
+        assert isinstance(message, SupportedAppProtocolRes)
+
+    def test_common_SupportedAppProtocolReq_messages_can_be_parsed(self):
+        decoded_dict = json.loads('{"supportedAppProtocolReq":{"AppProtocol":[{"ProtocolNamespace":"urn:iso:15118:2:'
+                                  '2013:MsgDef","VersionNumberMajor":2,"VersionNumberMinor":0,"SchemaID":0,"Priority"'
+                                  ':1}]}}',
+                                  cls=CustomJSONDecoder)
+
+        message = SupportedAppProtocolReq.parse_obj(decoded_dict["supportedAppProtocolReq"])
+        assert isinstance(message, SupportedAppProtocolReq)
+
+
