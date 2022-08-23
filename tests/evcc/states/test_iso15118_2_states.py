27fbde88fc04bcdadfb9806169a895059eab6b38
SudeepthiP36 <107793379+SudeepthiP36@users.noreply.github.com>
2022-08-23 09:29:18 +0100

    Update test_iso15118_2_states.py

--- tests/evcc/states/test_iso15118_2_states.py
+++ tests/evcc/states/test_iso15118_2_states.py
@@ -6,6 +6,9 @@ from iso15118.evcc.states.iso15118_2_states import (
     CurrentDemand,
     PowerDelivery,
     WeldingDetection,
+    ServiceDiscovery,
+    PaymentServiceSelection,
+    ServiceDetail
 )
 from iso15118.shared.messages.iso15118_2.datatypes import ChargingSession
 from iso15118.shared.notifications import StopNotification
@@ -13,8 +16,11 @@ from tests.evcc.states.test_messages import (
     get_v2g_message_current_demand_res,
     get_v2g_message_current_demand_res_with_stop_charging,
     get_v2g_message_power_delivery_res,
+    get_v2g_message_service_discovery_res_without_service_list,
+    get_service_discovery_response_code_not_ok,
+    get_v2g_message_service_discovery_res_with_service_list
 )
-
+from iso15118.shared.states import Terminate
 
 @patch("iso15118.shared.states.EXI.to_exi", new=Mock(return_value=b"01"))
 @pytest.mark.asyncio
@@ -58,3 +64,29 @@ class TestEvScenarios:
             message=get_v2g_message_power_delivery_res()
         )
         assert power_delivery.next_state == WeldingDetection
+        
+    async def test_service_discovery_to_payment_service_selection_when_no_service_list(self):
+        # V2G2-490
+        service_discovery = ServiceDiscovery(self.comm_session)
+        await service_discovery.process_message(
+            message=get_v2g_message_service_discovery_res_without_service_list()
+        )
+
+        assert service_discovery.next_state == PaymentServiceSelection
+
+    async def test_service_discovery_to_payment_service_selection_with_service_list_in_response(self):
+        # V2G2-489
+        service_discovery = ServiceDiscovery(self.comm_session)
+        await service_discovery.process_message(
+            message=get_v2g_message_service_discovery_res_with_service_list()
+        )
+        assert service_discovery.next_state == ServiceDetail
+
+    async def test_service_discovery_response_not_ok(self):
+        # V2G2-488
+        self.comm_session.stop_reason = StopNotification(True, "pytest")
+        service_discovery = ServiceDiscovery(self.comm_session)
+        await service_discovery.process_message(
+            message=get_service_discovery_response_code_not_ok()
+        )
+        assert service_discovery.next_state == Terminate
