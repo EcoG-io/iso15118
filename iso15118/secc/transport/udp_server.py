8228f7c35b6ceaa9a11b3237134c1385b3e4fe99
ikaratass <89934937+ikaratass@users.noreply.github.com>
2022-11-21 09:17:29 +0000

    rollback unrelated changes

--- iso15118/secc/transport/udp_server.py
+++ iso15118/secc/transport/udp_server.py
@@ -63,9 +63,8 @@ class UDPServer(asyncio.DatagramProtocol):
         # Bind the socket to the predefined port for receiving
         # UDP packets (SDP requests)
         full_ipv6_address = await get_link_local_full_addr(SDP_SERVER_PORT, iface)
-        logger.info(full_ipv6_address)
-        #sock.bind(full_ipv6_address)
-        sock.bind(("", SDP_SERVER_PORT))
+        sock.bind(full_ipv6_address)
+
         # After the regular socket is created and bound to a port, it can be
         # added to the multicast group by using setsockopt() to set the
         # IPV6_JOIN_GROUP option. The option value is the 16-byte packed
