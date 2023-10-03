0bcf5e05b7eab645ee41da6d28d6a3ac17e3dcf7
ikaratass <89934937+ikaratass@users.noreply.github.com>
2023-10-03 18:35:00 +0100

    code quality

--- iso15118/shared/security.py
+++ iso15118/shared/security.py
@@ -154,9 +154,7 @@ def get_ssl_context(server_side: bool) -> Optional[SSLContext]:
             )
             return None
         except FileNotFoundError:
-            logger.exception(
-                f"Can't find certfile or keyfile for SSL context"
-            )
+            logger.exception(f"Can't find certfile or keyfile for SSL context")
             return None
         except Exception as exc:
             logger.exception(exc)
@@ -474,7 +472,7 @@ def log_certs_details(certs: List[bytes]):
             f"Validity: {der_cert.not_valid_before} - {der_cert.not_valid_after}"
         )
         logger.debug(
-            f"Fingerprint: {der_cert.fingerprint(der_cert.signature_hash_algorithm).hex(':')}" # noqa
+            f"Fingerprint: {der_cert.fingerprint(der_cert.signature_hash_algorithm).hex(':')}"  # noqa
         )
         logger.debug("===")
 
